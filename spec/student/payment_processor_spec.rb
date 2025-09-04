3296a07b7d2e97e4e8206672ce8ba56d3ab046f4
Kyle <kylegranthousel@gmail.com>
2025-09-04 10:15:14 -0400

    Use doubles and spies

--- spec/student/payment_processor_spec.rb
+++ spec/student/payment_processor_spec.rb
@@ -1,5 +1,30 @@
 # frozen_string_literal: true
 
 describe PaymentProcessor do
-  # ...student specs...
+
+  describe "#initialize"
+    it "initializes with a gateway and logger" do
+      processor = instance_double("PaymentProcessor", gateway: "venmo")
+      allow(processor).to receive(:gateway).and_return("venmo")
+      allow(processor).to receive(:logger).and_return(nil)
+    end
+
+  describe "#process"
+    it "returns a result with valid arguments" do
+
+    end
+
+  describe "#process" do
+    it "charges the gateway and logs info" do
+      gateway = double("Gateway", charge: "ok")
+      logger = spy("Logger")
+      processor = PaymentProcessor.new(gateway: gateway, logger: logger)
+
+      result = processor.process(100, "acct_123")
+
+      expect(result).to eq("ok")
+      expect(logger).to have_received(:info).with("Charged 100 to acct_123")
+    end
+  end
+
 end
