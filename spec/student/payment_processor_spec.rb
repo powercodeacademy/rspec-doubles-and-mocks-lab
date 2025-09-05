60543fe7f70b4eeef7df708dbef8d105a56d3524
Shantel <Shantelgray24@gmail.com>
2025-09-05 13:50:34 -0400

    Recieve Gateway

--- spec/student/payment_processor_spec.rb
+++ spec/student/payment_processor_spec.rb
@@ -5,7 +5,7 @@ describe PaymentProcessor do
  context "process payment was successful" do
     it "charge the gateay correctly" do
       gateway = double("Gateway")
-      allow(gateway).to recieve(:charge)
+      allow(gateway).to receive(:charge)
       processor = PaymentProcessor.new(gateway: gateway, logger: logger)
       processor.process(amount, amount)
       expect(gateway).to have_received(:charge).with(amount, amount)
