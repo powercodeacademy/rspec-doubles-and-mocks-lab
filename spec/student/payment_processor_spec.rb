a7ed73d101ad430342c6eae6a4bac9dcb514476c
Shantel <Shantelgray24@gmail.com>
2025-09-05 14:52:27 -0400

    Payment Charge was Successful

--- spec/student/payment_processor_spec.rb
+++ spec/student/payment_processor_spec.rb
@@ -5,7 +5,9 @@ describe PaymentProcessor do
  context "process payment was successful" do
     it "charge the gateway correctly" do
       gateway = double("Gateway")
-      allow(gateway).to receive(:charge)
+      allow(gateway).to receive(:charge).and_return("success")
+      payment_processor = double("PaymentProcessor")
+      allow(payment_processor).to receive(:process)
       processor = PaymentProcessor.new(gateway: gateway)
       processor.process(52, "shantel_324")
       expect(gateway).to have_received(:charge).with(52, "shantel_324")
