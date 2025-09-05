d653cedd0e28bbb4d2f5bd4d9815374776680948
Shantel <Shantelgray24@gmail.com>
2025-09-05 13:49:27 -0400

    Processing Gateway

--- spec/student/payment_processor_spec.rb
+++ spec/student/payment_processor_spec.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 describe PaymentProcessor do
-  # ...student specs...
+  
+ context "process payment was successful" do
+    it "charge the gateay correctly" do
+      gateway = double("Gateway")
+      allow(gateway).to recieve(:charge)
+      processor = PaymentProcessor.new(gateway: gateway, logger: logger)
+      processor.process(amount, amount)
+      expect(gateway).to have_received(:charge).with(amount, amount)
+    end
+  end
 end
